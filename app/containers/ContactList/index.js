b6a8de71a790668075b9160a23416bf95e77a320
Blake Fletcher <blkfltchr@gmail.com>
2019-05-16 15:41:15 -0400

    Added tabs and searchbar for contact filtering

--- app/containers/ContactList/index.js
+++ app/containers/ContactList/index.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
 import { API, graphqlOperation } from 'aws-amplify';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
 import ContactCard from '../ContactCard';
+import SearchBar from '../../components/SearchBar';
 
 const ListContacts = `
 query list {
@@ -12,9 +17,22 @@ query list {
   }
 `;
 
+const styles = {
+  root: {
+    maxWidth: 600,
+    width: '90%',
+    margin: '0 auto',
+  },
+  tab: {
+    maxWidth: 100,
+    width: '22%',
+  },
+};
+
 class ContactList extends Component {
   state = {
     contacts: [],
+    value: 0,
   };
 
   async componentDidMount() {
@@ -22,13 +40,31 @@ class ContactList extends Component {
     this.setState({ contacts: contacts.data.listContacts.items });
   }
 
+  handleChange = (event, value) => {
+    this.setState({ value });
+  };
+
   render() {
+    const { classes } = this.props;
     if (!this.state.contacts) {
-      return <h2>Loading...</h2>;
+      return <h2>Please add a contact...</h2>;
     }
     return (
-      <div>
+      <div className={classes.root}>
         <h2>Contacts</h2>
+        <Tabs
+          value={this.state.value}
+          onChange={this.handleChange}
+          indicatorColor="primary"
+          textColor="primary"
+          centered
+        >
+          <Tab className={classes.tab} label="All" />
+          <Tab className={classes.tab} label="Friends" />
+          <Tab className={classes.tab} label="Family" />
+          <Tab className={classes.tab} label="Network" />
+        </Tabs>
+        <SearchBar />
         {this.state.contacts.map(contact => (
           <ContactCard key={contact.id} contact={contact} />
         ))}
@@ -37,4 +73,8 @@ class ContactList extends Component {
   }
 }
 
-export default ContactList;
+ContactList.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(ContactList);
